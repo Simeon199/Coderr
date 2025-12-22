dc9f272671ec42a51637da26d19fdf0d3303070f
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2025-12-22 22:17:34 +0100

    changes in auth_app and profile_app added

--- profile_app/models.py
+++ profile_app/models.py
@@ -3,17 +3,14 @@ from django.conf import settings
 # from django.contrib.auth.models import User
 
 class AbstractProfile(models.Model):
-    # username = models.CharField(max_length=100)
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name='%(class)s_profile',
-        blank=True,
-        null=True
     )
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    file = models.CharField(max_length=100, blank=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    file = models.CharField(max_length=100, null=True)
 
     class Meta:
         abstract = True
@@ -25,10 +22,10 @@ class CustomerProfile(AbstractProfile):
     pass
 
 class BusinessProfile(AbstractProfile):
-    location = models.CharField(max_length=100)
-    tel = models.CharField(max_length=20)
-    description = models.TextField()
-    working_hours = models.CharField(max_length=50)
+    location = models.CharField(max_length=100, blank=True)
+    tel = models.CharField(max_length=20, blank=True)
+    description = models.TextField(blank=True)
+    working_hours = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
-        return f"{self.username} - {self.location}" 
+        return f"{self.user.username} - {self.location}" 
