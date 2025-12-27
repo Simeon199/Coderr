f169ab289106b66445538d5d55e565521d44b560
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2025-12-27 20:46:58 +0100

    feat: new endpoint (for creating offers) added to urls.py in offers_app

--- offers_app/api/urls.py
+++ offers_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OffersListView
+from .views import OffersListView, OfferCreateUpdateView
 
 urlpatterns = [
-    path('offers/', OffersListView.as_view(), name='offers-list')
+    path('offers/', OffersListView.as_view(), name='offers-list'),
+    path('offers/create/', OfferCreateUpdateView.as_view(), name='offer-create')
 ]
