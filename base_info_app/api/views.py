0e40d9dfdbdc90dee8934003b39775d784472af4
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-01-10 20:30:10 +0100

    feat: GET request added to the BaseInfo endpoint of the base_info_app

--- base_info_app/api/views.py
+++ base_info_app/api/views.py
@@ -1,5 +1,22 @@
-from base_info_app.models import BaseInfo
+from django.db.models import Count, Avg
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from reviews_app.models import Review
+from offers_app.models import Offer
+from profile_app.models import BusinessProfile
 
 class BaseInfoView(APIView):
-    pass
+    def get(self, request):
+        review_count = Review.objects.count()
+        average_rating = Review.objects.aggregate(Avg('rating'))['rating__avg'] or 0
+        business_profile_count = BusinessProfile.objects.count()
+        offer_count = Offer.objects.count()
+
+        data = {
+            'review_count': review_count,
+            'average_rating': average_rating,
+            'business_profile_count': business_profile_count,
+            'offer_count': offer_count,
+        }
+
+        return Response(data)
--- base_info_app/models.py
+++ base_info_app/models.py
@@ -1,9 +1,4 @@
 from django.db import models
 
-# Links to the related objects are missing for the moment
-
 class BaseInfo(models.Model):
-    review_count = models.IntegerField()
-    average_rating = models.FloatField()
-    business_profile_count = models.IntegerField()
-    offer_count = models.IntegerField()
+    pass
